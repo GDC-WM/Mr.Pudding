b8a50ddc12ecd3a14605b944e20cf5f23df27468
Pablo Ibarz <fribarz123@gmail.com>
2024-03-01 13:53:04 -0500

    Made code organization more resilient to extension

--- Scripts/Player/PuddingMovement.gd
+++ Scripts/Player/PuddingMovement.gd
@@ -29,16 +29,16 @@ func _process(delta):
 	
 	update_axes()
 	
-	state.current_vel.x = accelerate(
-		state.current_vel.x, 
-		state.desired_direction.x, 
+	#apply horizontal acceleration
+	state.accelerate(
+		0,
 		-SPEED, SPEED, 
 		GROUND_ACCEL, GROUND_ACCEL * DECELLERATION_SCALE, 
 		delta
 	)
-	state.current_vel.y = accelerate(
-		state.current_vel.y, 
-		state.desired_direction.y, 
+	#apply vertical acceleration
+	state.accelerate(
+		1,
 		-FALL_SPEED, JUMP_SPEED, 
 		FALL_ACCEL, FALL_ACCEL, 
 		delta
@@ -60,49 +60,38 @@ func update_desired_direction(delta):
 	var jump := Input.is_action_pressed("ui_accept")
 	
 	#say that we want some horizontal movement
-	state.desired_direction.x = (-1 if left else 0) + (1 if right else 0)
+	state.desired_direction[0] = (-1 if left else 0) + (1 if right else 0)
 	
-	if (is_on_wall() && get_wall_normal().dot(state.current_horizontal * state.desired_direction.x) < 0.0):
-		state.desired_direction.x = 0.0
+	if (is_on_wall() && get_wall_normal().dot(state.current_horizontal * state.desired_direction[0]) < 0.0):
+		state.desired_direction[0] = 0.0
 	
 	state.grounded = is_on_floor()
 	if state.grounded: state.cur_cayote = 0.0
 	
 	#if they are, check for jumps
 	if state.grounded || state.cur_cayote < CAYOTE_TIME:
-		state.desired_direction.y = 0
-		state.current_vel.y = 0.0
-		state.jump_height = 0.0
-		state.jump_hang = 0.0
+		state.hold_on_ground()
 		#if jumping, instantly hit jumping speed
 		if jump:
-			state.current_vel.y = JUMP_SPEED
-			state.desired_direction.y = 1
-			state.grounded = false
-			state.cur_cayote = INF
+			state.start_jump_on_ground(JUMP_SPEED)
 			print("jump")
 	#if in the air, count towards the jump height and jump hang to see if the player should fall
 	else:
 		if !jump:
-			state.jump_height = INF
-			state.jump_height = INF
-		
-		if state.jump_height < JUMP_HEIGHT && jump:
-			state.current_vel.y = JUMP_SPEED
-			state.desired_direction.y = 1
+			state.end_jump_in_air()
+			
+		if state.jump_height < JUMP_HEIGHT:
+			state.hold_jump_in_air(JUMP_SPEED)
 			print("rise")
-		elif state.jump_height >= JUMP_HEIGHT && state.jump_hang < JUMP_HANG && jump:
-			state.jump_height = INF
-			state.jump_hang += delta
-			state.desired_direction.y = 0
+		elif state.jump_height >= JUMP_HEIGHT && state.jump_hang < JUMP_HANG:
+			state.hang_in_air(delta)
 			print("hang")
 		else:
-			state.jump_hang = INF
-			state.jump_height = INF
-			state.desired_direction.y = -1
+			state.end_jump_in_air()
+			state.desired_direction[1] = -1
 			print("fall")
 		
-		if (state.current_vel.y > 0): state.jump_height += state.current_vel.y * delta
+		state.update_jump_height(delta)
 		
 	state.cur_cayote += delta
 
@@ -110,16 +99,3 @@ func update_axes():
 	if state.grounded:
 		var n := get_floor_normal()
 		state.current_horizontal = Vector2(-n.y, n.x)
-
-func accelerate(from:float, axis:float, min:float, max:float, deaccel:float, accel:float, delta:float) -> float:
-	var d := signf(axis)
-	var a := accel if d == 1.0 else deaccel
-	
-	if (d == 0.0): d = -signf(from) #accelerate towards zero if the axis is nuetral
-	
-	from += d * delta * a
-	from = clampf(from, min, max)
-	
-	if (absf(from) < 10): from = 0.0 #set to zero if close to zero
-	
-	return from
--- Scripts/Player/PuddingState.gd
+++ Scripts/Player/PuddingState.gd
@@ -1,9 +1,9 @@
 extends Object
 class_name PuddingState
 
-var desired_direction := Vector2.ZERO
+var desired_direction:PackedFloat64Array = [0.0, 0.0]
 
-var last_desired_direction := Vector2.ZERO
+var last_desired_direction:PackedFloat64Array = [0.0, 0.0]
 var direction_changed:Array[bool] = [false, false]
 var grounded := false
 
@@ -12,7 +12,7 @@ var jump_hang := INF
 
 var cur_cayote := 0.0
 
-var current_vel := Vector2.ZERO
+var current_vel:PackedFloat64Array = [0.0, 0.0]
 
 var current_horizontal := Vector2.RIGHT
 var current_vertical := Vector2.UP
@@ -23,7 +23,54 @@ func update(delta):
 	last_desired_direction = desired_direction
 
 func get_velocity() -> Vector2:
-	return current_vel.x * current_horizontal + current_vel.y * current_vertical
+	return current_vel[0] * current_horizontal + current_vel[1] * current_vertical
 
-func set_velocity(v:Vector2) -> void:
-	current_vel = Transform2D(current_horizontal, current_vertical, Vector2.ZERO).inverse() * v
+func accelerate(along:int, min:float, max:float, deaccel:float, accel:float, delta:float):
+	current_vel[along] = accelerate_component(
+		current_vel[along], 
+		desired_direction[along], 
+		min, max, deaccel, accel, delta
+	)
+
+func accelerate_component(from:float, axis:float, min:float, max:float, deaccel:float, accel:float, delta:float) -> float:
+	var d := signf(axis)
+	var a := accel if d == 1.0 else deaccel
+	
+	if (d == 0.0): d = -signf(from) #accelerate towards zero if the axis is nuetral
+	
+	from += d * delta * a
+	from = clampf(from, min, max)
+	
+	if (absf(from) < 10): from = 0.0 #set to zero if close to zero
+	
+	return from
+
+func hold_on_ground():
+	desired_direction[1] = 0
+	current_vel[1] = 0.0
+	jump_height = 0.0
+	jump_hang = 0.0
+
+func start_jump_on_ground(speed:float):
+	current_vel[1] = speed
+	desired_direction[1] = 1
+	grounded = false
+	cur_cayote = INF
+
+func hold_jump_in_air(speed:float):
+	current_vel[1] = speed
+	desired_direction[1] = 1
+
+func hang_in_air(delta:float):
+	jump_height = INF
+	jump_hang += delta
+	desired_direction[1] = 0
+
+
+
+func end_jump_in_air():
+	jump_height = INF
+	jump_hang = INF
+
+func update_jump_height(delta:float):
+	if (current_vel[1] > 0): jump_height += current_vel[1] * delta
