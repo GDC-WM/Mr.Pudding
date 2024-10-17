32c974ecc031bb17e596319dd2a6cba540725bb9
Pablo Ibarz <fribarz123@gmail.com>
2024-10-16 20:21:44 -0400

    "That feels pretty good"

--- Scripts/Player/PuddingMovement.gd
+++ Scripts/Player/PuddingMovement.gd
@@ -27,13 +27,13 @@ const DASH_TIME := 0.25
 #multiplier for acceleration values when turning around
 const DECELLERATION_SCALE := 5.0
 #multiplier for acceleration values when exceeding SPEED 
-const WALK_PULL_SCALE := 0.1
+const WALK_PULL_SCALE := 1.0
 
 #time the player can stand off of a platform before being considered no longer grounded
 const CAYOTE_TIME := 0.1
 
-const RUN_SPEED := 2000.0
-const RUN_ACCEL := 800.0
+const RUN_SPEED := 1500.0
+const RUN_ACCEL := 1000.0
 
 #the state of the player
 var state := PuddingState.new()
@@ -44,9 +44,6 @@ func _process(delta):
 	if Input.is_action_pressed("dash") and state.movement_type == state.MOVEMENT_TYPE.WALK:
 		state.movement_type = state.MOVEMENT_TYPE.DASH
 		state.dash_time = 0.0
-	
-	if !Input.is_action_pressed("sprint") and state.movement_type == state.MOVEMENT_TYPE.RUN:
-		state.movement_type = state.MOVEMENT_TYPE.WALK
 	
 	#apply player input to state based on the movement type
 	match state.movement_type:
@@ -123,6 +120,7 @@ func update_walk(delta):
 	if (is_pressing_wall()):
 		state.desired_direction[0] = 0.0
 	else:
+		if Input.is_action_pressed("sprint"): state.movement_type = state.MOVEMENT_TYPE.RUN
 		state.desired_direction[0] = (-1 if left else 0) + (1 if right else 0)
 	
 	update_jump(delta, jump)
@@ -160,7 +158,7 @@ func update_dash(delta):
 	
 	state.dash_time += delta
 	if (state.dash_time >= DASH_TIME || is_pressing_wall()):
-		state.movement_type = state.MOVEMENT_TYPE.RUN
+		state.movement_type = state.MOVEMENT_TYPE.WALK
 		return
 	
 	state.current_vel[0] = state.get_facing(0) * DASH_SPEED
@@ -168,10 +166,12 @@ func update_dash(delta):
 
 #after a dash the player is sprinting
 func update_run(delta):
-	update_grounded()
+	print("bruh")
+	
+	update_jump(delta, Input.is_action_pressed("ui_accept"))
 	
 	#implement wall running later, lol. If I crash into a walk, return to walking mode, which has cases for running into walls
-	if (is_pressing_wall()):
+	if (is_pressing_wall() or !Input.is_action_pressed("sprint")):
 		state.movement_type = state.MOVEMENT_TYPE.WALK
 		return
 	
@@ -181,14 +181,17 @@ func update_run(delta):
 	
 	state.desired_direction[0] = (-1 if left else 0) + (1 if right else 0)
 	
-	#give the player a speed boost if they land within walking speed while sprinting
-	if signf(state.desired_direction[0]) == signf(state.current_vel[0]) && state.axis_pull(state.current_vel[0], -SPEED, SPEED) == 0:
-		state.current_vel[0] *= RUN_SPEED / SPEED
-	
 	state.accelerate(
 		0,
 		-RUN_SPEED, RUN_SPEED, 
-		RUN_ACCEL, RUN_ACCEL, 
+		RUN_ACCEL * DECELLERATION_SCALE, RUN_ACCEL, 
+		delta
+	)
+	
+	state.accelerate(
+		1,
+		-FALL_SPEED, JUMP_SPEED, 
+		FALL_ACCEL, FALL_ACCEL, 
 		delta
 	)
 	
