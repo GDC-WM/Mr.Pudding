0e2abcc037834362b9dea599c58d6ca390178344
Pablo Ibarz <fribarz123@gmail.com>
2024-11-27 20:16:10 -0500

    Added some basic running mechanics

--- Scripts/Player/PuddingMovement.gd
+++ Scripts/Player/PuddingMovement.gd
@@ -2,16 +2,11 @@ extends CharacterBody2D
 
 #speed at which the player can run in pixels per second
 const SPEED := 500.0
-#acceleration due to movement, pixels per second squared
-const GROUND_ACCEL := 6000.0
-const AIR_ACCEL := 3000.0
 
-const MAX_FLOOR_NORMAL := 0.395
-const MAX_STEP := 610
+const MAX_FLOOR_Y := -0.5
 
 #acceleration due to gravity, pixels per second squared
 const FALL_ACCEL := 4000.0
-const FASTFALL_ACCEL_MULTIPLIER := 1.5
 #speed at which the player can fall in pixels per second
 const FALL_SPEED := 1400.0
 
@@ -22,70 +17,140 @@ const JUMP_HEIGHT := 75.0
 #hang time in the air in seconds while holding jump
 const JUMP_HANG := -INF
 
-#multiplier for acceleration values when turning around
-const DECELLERATION_SCALE := 5.0
-const RUN_DECEL_SCALE := 2.0
-#multiplier for acceleration values when exceeding SPEED 
-const WALK_PULL_SCALE := 0.5
-
 #time the player can stand off of a platform before being considered no longer grounded
 const CAYOTE_TIME := 0.1
 
-const RUN_SPEED := 1500.0
-const RUN_ACCEL := 1000.0
+const RUN_SPEED := 2000.0
+const MAX_FLOOR_Y_RUN := 0.0
+
+const RUN_ACCEL := 500.0
+var current_run_speed := SPEED #isolated from player state because I wanted to keep the implementation separate
+
+#delay run a little, just a small amount, 
+const RUN_DELAY := 0.1
 
 #the state of the player
 var state := PuddingState.new()
 
 @onready var casts:Node2D = get_node("Cast")
 @onready var cast_down:ShapeCast2D = get_node("Cast/CastDown")
-@onready var cast_forward:RayCast2D = get_node("Cast/CastForward")
-@onready var cast_latch:RayCast2D = get_node("Cast/CastLatch")
+@onready var cast_forward:ShapeCast2D = get_node("Cast/CastForward")
+@onready var cast_latch:ShapeCast2D = get_node("Cast/CastLatch")
+
+@onready var run_timer:Timer = get_node("RunTimer")
 
 func accelerate(from:float, to:float, accel:float, delta:float) -> float:
 	return from + signf(to - from) * delta * accel
 
-func get_last_collision_highest_normal(cast:ShapeCast2D) -> Dictionary:
-	var best := {"normal":Vector2.ZERO}
-	for result in cast_down.collision_result:
-		if (result["normal"].y > best["normal"].y):
+func get_last_highest_normal(cast:ShapeCast2D) -> Dictionary:
+	var best := {"normal":Vector2.DOWN}
+	for result in cast.collision_result:
+		if result["normal"].y < best["normal"].y:
 			best = result
 	return best
 
-func update_grounded(cast_collision:Dictionary, self_collision:KinematicCollision2D) -> bool:
-	
-	if state.grounded:
-		#get the distance of the cast collision along the state's normal,
-		#if less than step, make the step, otherwise, fall
-		return true
-	else:
-		#the player is in the air, only use their direct collisions
-		if (self_collision == null): return false
-		
-		#if the player is running, grab the wall
-		
-		#if the player is not running, only grab the wall within a certain normal
-		var c_norm:Vector2 = self_collision.get_normal()
-		if (c_norm.y <= MAX_FLOOR_NORMAL):
-			return true
-		else: 
-			return false
+func get_last_closest_point(cast:ShapeCast2D) -> Dictionary:
+	var best := {}
+	var best_distance := INF
+	for result in cast.collision_result:
+		var d:float = position.distance_to(result["point"])
+		if d < best_distance:
+			best = result
+			best_distance = d
+	return best
 
 func _process(delta):
-	state.internal_velocity[0] = Input.get_axis("ui_left", "ui_right") * SPEED
-	
-	state.grounded = update_grounded(
-		get_last_collision_highest_normal(cast_down), 
-		get_last_slide_collision()
-	)
+	state.update_movement_axis(Vector2(
+		Input.get_axis("ui_left", "ui_right"),
+		Input.get_axis("ui_down", "ui_up")
+	))
 
-	if (!state.grounded):
-		state.internal_velocity[1] = accelerate(state.internal_velocity[1], FALL_SPEED, FALL_ACCEL, delta)
-	else:
-		state.internal_velocity[1] = 0
+func handle_walk(delta):
 	
+	#check if the player is touching something they could land on
+	state.try_ground(
+		get_last_slide_collision(),
+		MAX_FLOOR_Y
+	)
+	
+	if state.grounded: 
+		state.try_cast_down(get_last_highest_normal(cast_down), MAX_FLOOR_Y, SPEED)
+			
+	if state.grounded:
+		if state.movement_axis == Vector2.ZERO:
+			state.v2 = Vector2.ZERO
+		
+		#if the player is grounded, walk along the slope
+		var t := state.last_tangent()
+		state.v1 = t * state.movement_axis[0] * SPEED
+		#also allow a sprint to start instantly
+		if state.v1 != Vector2.ZERO && \
+			Input.is_action_pressed("sprint"):
+			state.movement_type = state.MOVEMENT_TYPE.RUN
+	else:
+		#if the player is not grounded, still allow them freedom of horizontal movement
+		state.v1 = Vector2(state.movement_axis[0] * SPEED, 0.0)
+		
+		#accelerate the player towards the ground
+		state.v2[0] = 0.0
+		state.v2[1] += accelerate(state.v1[1], FALL_SPEED, FALL_ACCEL, delta)
+		if state.v2[1] != clampf(state.v2[1], -FALL_SPEED, FALL_SPEED):
+			state.v2[1] = FALL_SPEED * signf(state.v2[1])
+
+func handle_run(delta):
+	
+	#no pausing
+	if (state.movement_axis[0] == 0.0):
+		state.movement_axis[0] = state.facing[0]
+		
+	if state.grounded: 
+		state.try_cast_down(get_last_highest_normal(cast_down), MAX_FLOOR_Y, SPEED)
+	
+	var result := get_last_closest_point(cast_forward)
+	var normal = result["normal"] if result.has("normal") else null
+	if normal && normal.y <= MAX_FLOOR_Y_RUN:
+		state.last_normal = normal
+		state.v2 = -normal * RUN_SPEED
+		state.grounded = true
+	#if we face away from the last wall, which was too steep to walk on, fall
+	#this feels better because walls have an easy escape, but floors are still floors
+	elif state.last_normal.y > MAX_FLOOR_Y && (state.last_normal * state.movement_axis).x < 0.0:
+		state.grounded = false
+
+	print(current_run_speed)
+
+	if state.grounded:
+		#if the player is grounded, walk along the slope
+		var t := state.last_tangent()
+		state.v1 = t * state.movement_axis[0] * current_run_speed
+		#allow returning to walk instantaneously
+		if state.facing.x != state.movement_axis.x || \
+			!Input.is_action_pressed("sprint"):
+			
+			current_run_speed = SPEED
+			state.movement_type = state.MOVEMENT_TYPE.WALK
+	else:
+		#if the player is not grounded, still allow them freedom of horizontal movement
+		state.v1 = Vector2(state.movement_axis[0] * RUN_SPEED, 0.0)
+		
+		#accelerate the player towards the ground
+		state.v2[0] = 0.0
+		state.v2[1] += accelerate(state.v1[1], FALL_SPEED, FALL_ACCEL, delta)
+		if state.v2[1] != clampf(state.v2[1], -FALL_SPEED, FALL_SPEED):
+			state.v2[1] = FALL_SPEED * signf(state.v2[1])
+
+	current_run_speed += RUN_ACCEL * delta
+	current_run_speed = clampf(current_run_speed, 0.0, RUN_SPEED)
 
 func _physics_process(delta):
-	velocity = state.internal_velocity
+	
+	match state.movement_type:
+		state.MOVEMENT_TYPE.WALK:
+			handle_walk(delta)
+		state.MOVEMENT_TYPE.RUN:
+			handle_run(delta)
+	
+	velocity = state.get_velocity()
 	#print(velocity)
 	move_and_slide()
+	casts.scale.x = state.facing.x
--- Scripts/Player/PuddingState.gd
+++ Scripts/Player/PuddingState.gd
@@ -1,18 +1,68 @@
 extends Object
 class_name PuddingState
 
-enum MOVEMENT_MODE {
+enum MOVEMENT_TYPE {
 	WALK,
+	RUN
 }
 
-var internal_velocity := Vector2(0.0,0.0)
+#v1 is "instant" velocity and v2 is "physics" velocity
+#on any given physics tick, the player should move the sum of these two
+var v1 := Vector2.ZERO
+var v2 := Vector2.ZERO
+func get_velocity() -> Vector2:
+	return v1 + v2
 
-var movement_type := MOVEMENT_MODE.WALK
+var facing := Vector2.ONE
+var movement_axis := Vector2.ZERO
+
+func update_movement_axis(to:Vector2):
+	if (to.x != 0.0): facing.x = signf(to.x)
+	if (to.y != 0.0): facing.y = signf(to.y)
+	movement_axis = to
+
+var movement_type := MOVEMENT_TYPE.WALK
 
 var grounded := false
 var pushing_wall := false
 
 var last_normal = Vector2.UP
+
+func last_tangent() -> Vector2:
+	return Vector2(-last_normal.y, last_normal.x)
+
+#try to land the player, assuming they are not grounded
+func try_ground(self_collision:KinematicCollision2D, max_floor_y:float):
+	if grounded:
+		return
+	else:
+		#the player is in the air, only use their direct collisions
+		if (self_collision == null): 
+			grounded = false
+			return
+		
+		#only grab the terrain within a certain normal
+		var c_norm:Vector2 = self_collision.get_normal()
+		if (c_norm.y <= max_floor_y):
+			grounded = true
+			last_normal = c_norm
+			return
+		else:
+			grounded = false
+			return
+
+#attempt to keep the player on the ground while they are in motion
+#supply collision data with the nearest point on the ground, the highest possible floor normal, and "stick_power," the speed with which the player attaches to the ground
+func try_cast_down(cast_down_collision:Dictionary, max_floor_y:float, stick_power:float):
+	var normal:Vector2 = cast_down_collision["normal"] if cast_down_collision.has("normal") else null
+	
+	if normal && normal.y <= max_floor_y:
+		last_normal = normal
+		#pull the player towards the ground
+		v2 = -normal * stick_power
+	#if they are not standing over anything, launch them back into the air
+	else:
+		grounded = false
 
 #draw different types of state variables with nice BBCode
 func draw_flag(x:bool) -> String:
@@ -27,8 +77,19 @@ func draw_float(x:float) -> String:
 		color = "red"
 	return "[color=" + color + "]" + str(x) + "[/color]"
 
+func draw_m_type(m:MOVEMENT_TYPE) -> String:
+	match m:
+		MOVEMENT_TYPE.WALK: return "[color=cyan]WALK[/color]"
+		MOVEMENT_TYPE.RUN: return "[color=orange]RUN[/color]"
+	return ""
+	
+
 func _to_string():
-	return "FLAGS:\n" + \
+	return "CATEGORICAL:\n" + \
 		"\t" + "grounded: " + draw_flag(grounded) + "\n" + \
-	"VARIABLES:\n" + \
-		"\t internal_velocity: (" + draw_float(internal_velocity.x) + ", " + draw_float(internal_velocity.y) + ")\n"
+		"\t" + "movement mode: " + draw_m_type(movement_type) + "\n" + \
+	"CONTINUOUS:\n" + \
+		"\t instant velocity: (" + draw_float(v1.x) + ", " + draw_float(v1.y) + ")\n" + \
+		"\t physics velocity: (" + draw_float(v2.x) + ", " + draw_float(v2.y) + ")\n" + \
+		"\t last normal: (" + draw_float(last_normal.x) + ", " + draw_float(last_normal.y) + ")\n" + \
+		"\t last tangent: (" + draw_float(last_tangent().x) + ", " + draw_float(last_tangent().y) + ")\n"
